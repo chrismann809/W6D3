095a6aa0a6da002ac87077f0455112499eae898e
danny <dannyho77@gmail.com>
2021-02-03 16:05:22 -0500

    completed userscontroller - create method

--- first_routes_and_controllers/app/controllers/users_controller.rb
+++ first_routes_and_controllers/app/controllers/users_controller.rb
@@ -1,2 +1,19 @@
 class UsersController < ApplicationController
+    def index
+        users = User.all
+        render json: users
+        # render json: params
+    end
+
+    def create
+        user = User.new(params.require(:user).permit(:name, :email))
+        # replace the `user_attributes_here` with the actual attribute keys
+        user.save!
+        render json: user
+    end
+
+    def show
+        
+        render json: params
+    end
 end
